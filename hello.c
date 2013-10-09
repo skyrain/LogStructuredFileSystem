baf8a60ecb0a94f371d4c54fc108c7031e09d251
skyrain <skyrain@skyrain-ThinkPad-X230.(none)>
2013-10-09 00:22:22 -0700

    log initial strucure

--- hello.c
+++ hello.c
@@ -9,7 +9,6 @@
 */
 
 #define FUSE_USE_VERSION 26
-//#define -D_FILE_OFFSET_BITS=64
 
 #include <fuse.h>
 #include <stdio.h>
--- log.h
+++ log.h
@@ -1,69 +1,92 @@
-#include "flash.h"a
+#include "flash.h"
 #include <time.h>
 #include <sys/types.h>
 
-#define BLOCKS_PER_SEG 16
+#define BLOCKS_PER_SEG 32
 #define BLOCK_SIZE FLASH_BLOCK_SIZE
 #define SEG_SIZE (BLOCKS_PER_SEG * BLOCK_SIZE)
+#define DIRECT_BK_NUM 4
 //#define N_BEGIN_BLOCK 2
 
-struct Block
+typedef struct Block
 {
     u_int block_size = BLOCK_SIZE;
     u_int block_no;
-}
+}Block;
 
-typedef struct Block_Usage
-{
-	int file_no;
-	int block_no;
-}Block_Usage; //store the info about is that block being used by which file.
-
+//store info of which block used by which file.
 typedef struct Seg_sum_entry
 {
     u_int file_no;              //should it be inode??
     u_int block_no;
 }Seg_sum_entry;
 
-typedef struct Seg_sum
+//Seg_num consists of Seg_sum_entry
+typedef struct Seg_sum_bk
 {
     u_int block_no;
-    Block_Usage *BlockUsageTable;
-    Seg_sum_entry* seg_sum_entry
-        = (Seg_sum_entry* )malloc(sizeof(Seg_sum_entry) * BLOCKS_PER_SEG);    
-}Seg_sum;
+    Seg_sum_entry* seg_sum_entry;
+}Seg_sum_bk;
 
-typedef struct Begin_block
+//Suppose only 1 Seg_sum_bk as Begin_bk in Seg
+typedef struct Begin_bk
 {
     u_int block_no;
-    Seg_num* begin_block = (Seg_num* )malloc(sizeof(Seg_num));
-}Begin_block;
+    Seg_sum_bk* begin_block;
+}Begin_bk;
 
-typedef struct Segment
+//segment definition
+typedef struct Seg
 {
-    Begin_block begin_block;
-    u_int seg_size = SEGMENT_SIZE;
+    Begin_bk begin_block;
+    u_int seg_size = SEG_SIZE;
     u_int seg_no;
-    //////////
 }
 
-
-struct seg_usage_table
+//for cleaning policy,record segment usage table
+//per segment linked to seg_usage_table
+typedef struct Seg_usage_table
 {
-    u_int NumLiveBlocks;
+    u_int seg_no;
+    u_int num_live_bk;
     //u_int num_live_bytes; // should not be num of live blocks???
     time_t modify_Time;
-}SegUsagT
+}Seg_usage_t;
 
-struct checkpoint_region
+
+//store metadata
+struct Checkpoint_region
 {
-    
+    //???address of all blocks in lfs or flash memory
+
+
 }
+
+//contains direct block info
+//seg_no: which seg this block it belongs to
+//bk_no: what block_no in the seg
+typedef struct Direct_bk
+{
+    u_int seg_no;
+    u_int bk_no;
+}Direct_bk;
+
+typedef struct Bk_list
+{
+    Direct_bk bk;
+    Bk_list* next;
+}Bk_list;
 
 typedef struct Inode
 {
-	int  ino;
-	int filesize;
+	u_int  ino;
+    u_int seg_no_in_log;//this inode is stored in which seg of log
+    u_int bk_no_in_log;//this inode is stored in which block of log
+    u_int filetype;
+	u_int filesize;
+    //flash address of the inode's blocks
+    Direct_bk[DIRECT_BK_NUM] direct_bk;
+    Bk_list indirect_bk;
 	int mode;
 	uid_t userID;
 	gid_t groupID;
@@ -72,12 +95,45 @@ typedef struct Inode
 	time_t create_Time;
 	time_t change_Time;
 	int num_links;
-}inode;
+}Inode;
 
+//store the inode and its corresponding location in log
+typedef struct Inode_location
+{
+    u_int ino;
+    u_int log_seg_no;
+    u_int log_bk_no;
+}Inode_location;
+
+typedef struct Inode_map_list
+{
+    Inode_location inode_location;
+    Inode_map_list* next;
+}Inode_map_list;
+
+//inode_no list, stores the inode's id
+
+//be written into log as block
+typedef struct Inode_map
+{
+   Inode_map_list inode_map_list; 
+}
+
+//
 struct log_beginning
 {
     u_int segment_size = FLASH_BLOCK_SIZE * BLOCKS_PER_SEGMENT;
     u_int block_size = FLASH_BLOCK_SIZE;
-    struct seg_usage_table seg_usage_table; 
-    
+    //how many segments in flash memory
+    //????????????????????????????
+    u_int SEG_NUM = ;
+    seg_usage_table[SEG_NUM] seg_usage_table;
+    Inode_map Inode_map;
+    //???anything else
 }
+
+
+
+
+
+
