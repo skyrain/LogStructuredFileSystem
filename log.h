6f51cc6264ff7af71d0eacdf7f92db8cd6da905c
Farmost East <octaw@sina.com>
2013-10-20 12:37:26 -0700

    Update log.h

--- log.h
+++ log.h
@@ -158,73 +158,8 @@ typedef struct Checkpoint_region
 
 
 /*  ---------------- in file.h--------------------------------
-
-//contains direct block info in disk
-typedef struct Block_pointer
-{
-    //------- file bk size in sectors--------
-    //------actually in flash memory: 1 sector is its 1 block-------
-    logAddress bk_log_addr;
-
-}Block_pointer;
-
-
-typedef struct Inode
-{
-    u_int ino;
-   
-//    u_int seg_no_in_log;//this inode is stored in which seg of log
-//    u_int bk_no_in_log;//this inode is stored in which block of log
-    u_int filetype;
-    u_int filesize;
-    char filename[FILE_NAME_LENGTH + 1]; //phase 1
-    
-    //flash address of the inode's blocks
-    Block_pointer direct_bk[DIRECT_BK_NUM];
-    //Bk_list indirect_bk;
-    //Block_pointer indirect_bk;  phase 2
-   
-    int mode;
-    mode_t mode;
-    uid_t userID;
-    gid_t groupID;
-    time_t modify_Time;
-    time_t access_Time;
-    time_t create_Time;
-    time_t change_Time;
-    int num_links;
-
-}Inode;
-
-//store the inode 
-typedef struct Inode_location
-{
-    Inode inode;
-    //offset in "ifile",
-    // there stores the inode for ino
-    u_int offset; 
-    struct Inode_location * next;
-
-}Inode_location;
-
+removed
 --------------------------------------------------------------*/
-
-
-
-/*------------- in directory.h-------------------------------------
-//Ifile stores the inode_location of each inode
-//in file "ifile"
-typedef struct Ifile
-{
-    char * name;
-    Inode_location * inode_loc;
-}Ifile;
-
------------------------------------------------------------------*/
-
-
-
-
 
 //super log segment 存整个log的信息和checkpoint等
 typedef struct Super_log_seg
