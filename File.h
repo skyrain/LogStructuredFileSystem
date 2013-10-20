18099cf1d2fc2c633c057c0f2cbf0a1f39398d64
Farmost East <octaw@sina.com>
2013-10-20 12:47:20 -0700

    Update File.h

--- File.h
+++ File.h
@@ -1,6 +1,5 @@
 /*
  * File.h
- * Author: Xudong Weng
  * Created on: Oct 11, 2013
  * This is the file layer header
  */
@@ -16,6 +15,55 @@
 
 //temp fun
 //int Log_Init(char *filename, Inode **ifile, int *closestatus);
+
+
+//contains direct block info in disk
+typedef struct Block_pointer
+{
+    //------- file bk size in sectors--------
+    //------actually in flash memory: 1 sector is its 1 block-------
+    int seg_num;
+    int bk_no;
+}Block_pointer;
+
+
+typedef struct Inode
+{
+    u_int ino;
+   
+//    u_int seg_no_in_log;//this inode is stored in which seg of log
+//    u_int bk_no_in_log;//this inode is stored in which block of log
+    u_int filetype;
+    u_int filesize;
+    char filename[FILE_NAME_LENGTH + 1]; //phase 1
+    
+    //flash address of the inode's blocks
+    Block_pointer direct_bk[DIRECT_BK_NUM];
+    //Bk_list indirect_bk;
+    //Block_pointer indirect_bk;  phase 2
+   
+    int mode;
+    mode_t mode;
+    uid_t userID;
+    gid_t groupID;
+    time_t modify_Time;
+    time_t access_Time;
+    time_t create_Time;
+    time_t change_Time;
+    int num_links;
+
+}Inode;
+
+//store the inode 
+typedef struct Inode_location
+{
+    Inode inode;
+    //offset in "ifile",
+    // there stores the inode for ino
+    u_int offset; 
+    struct Inode_location * next;
+
+}Inode_location;
 
 int File_Init(Inode *inode, int type);
 
