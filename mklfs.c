e8d1cf351a5deb0c3b32b14378e581f93ebcfe8c
skyrain <skyrain@skyrain-ThinkPad-X230.(none)>
2013-10-22 16:31:21 -0700

    mklfs.c

--- mklfs.c
+++ mklfs.c
@@ -1,41 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "flash.h"
 #include "log.h"
 
 //??----------还没有管 wearlimit-------------------------
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
     //erase bk size
     //----------------------还未用---------------
     u_int er_bk_size = 16;   
 
-//用户输入--------------------------------------------    
-    //？？之后会作为参数设置,此处为默认值
-    //用户输入
+    
+    //用户输入--------------------------------------------    
     wearlimit = 1000;
     fl_file = (char *)calloc(1, 8);
-    strcpy(fl_file, "File");          //flash memory name
+    strcpy(fl_file, "File");         
 
     //-----should always be 16 的整数倍
     sec_num = 1024;
-
     bk_size = 2;
     bks_per_seg = 32;
-    seg_size = bks_per_seg * bk_size;
-    
-    // 1024个sector, 32个block组成一个seg
-    //??注意应写程序保证若用户输入导致计算出的fl_seg_num非整数则让用户重新输入
-    seg_num = sec_num / bk_size / bks_per_seg;
-        
-    //根据用户输入定义log的相应结构大小
-   
-    bk_content_size = bk_size * FLASH_SECTOR_SIZE;
-    
+
+//??
+    //------------file layer 设置 cache---------------    
     //cache seg 数量，此处为默认值
     cache_seg_num = 4; 
- //用户输入--------------------------------------------------
+    //用户输入--------------------------------------------------
+
+/*  
+  mklfs [options] file
+    -l size, --segment=size
+        Segment size, in blocks. The default is 32.
+    -s sectors, --sectors=sectors
+        Size of the flash, in sectors.  The default is 1024.
+    -b size, --block=size
+        Size of a block, in sectors. The default is 2 (1KB).
+    -w limit, --wearlimit=limit
+        Wear limit for erase blocks. The default is 1000.
+
+*/
+    int ch;  
+    while ((ch = getopt(argc, argv, "l:s:b:w:")) != -1)  
+    {  
+        switch (ch) {  
+            case 'l': 
+                if(atoi(optarg) >= 65536)
+                {
+                    printf("Too many blocks per segment!\n");
+                    return 0;
+                } 
+                bks_per_seg = (u_int)atoi(optarg);
+                break;  
+            case 's':
+                if(atoi(optarg) >= 65536)
+                {
+                    printf("Too big flash memory!\n");
+                    return 0;
+                }
+                else
+                { 
+                    sec_num = (u_int)atoi(optarg);
+                }
+                break;  
+            case 'b':
+                bk_size = (u_int)atoi(optarg);
+                break;  
+            case 'w': 
+                if(atoi(optarg) >= 65536)
+                {
+                    printf("Too big wearlimit!\n");
+                }
+                wearlimit = (u_int)atoi(optarg);
+                break;  
+            case '?':
+                break;  
+        }
+    }  
+    strcpy(fl_file, argv[argc - 1]);
+
+ //??在多考虑其他边缘情况比如 sec_num = 0
+    seg_size = bks_per_seg * bk_size;
+    seg_num = sec_num / bk_size / bks_per_seg;
+    bk_content_size = bk_size * FLASH_SECTOR_SIZE;
+
+    if(sec_num % FLASH_SECTORS_PER_BLOCK != 0)
+    {
+        printf("Total sectors must be 16 whole times!\n");
+        return 0;
+    }
+
+    if(seg_size % FLASH_SECTORS_PER_BLOCK != 0)
+    {
+        printf("Seg size must be 16 whole times!\n");
+        return 0;
+    }
+    
+    if(seg_size > sec_num)
+    {
+        printf("Segment size exceed the entire flash memory!\n");
+        return 0;
+    }
+    
+    if(sec_num % bk_size != 0)
+    {
+        printf("Not whole number of blocks!\n");
+        return 0;
+    }
+
+    if((sec_num / bk_size) % bks_per_seg != 0)
+    {
+        printf("Not whole number of segs!\n");
+    }
 
 
 //-------------------------------------------------------------
