00c69d455adc91aea8928873d6d858928bc03ccb
skyrain <skyrain@skyrain-ThinkPad-X230.(none)>
2013-12-06 22:53:40 -0700

    add clean.c

--- log.h
+++ log.h
@@ -278,7 +278,7 @@ int read_cache(LogAddress  * logAddress, u_int length, void * buffer);
 //调用flash.h的函数
 int Log_Create();
 
-void copy_log_to_memory(int seg_no, Seg * seg);
+void copy_log_to_memory(int seg_no, void * copy_seg);
 
 void get_log_to_memory(LogAddress * log_addr);
 
